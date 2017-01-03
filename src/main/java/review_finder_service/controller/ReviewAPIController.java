b55f190d5656ad6a2dc523524a558c926ae3d68f
Leviathan11 <gubik.levente@gmail.com>
2017-01-03 13:27:08 +0100

    Service works as it supposed to

--- src/main/java/review_finder_service/controller/ReviewAPIController.java
+++ src/main/java/review_finder_service/controller/ReviewAPIController.java
@@ -3,6 +3,10 @@ package review_finder_service.controller;
 import review_finder_service.service.APIService;
 import spark.Request;
 import spark.Response;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+
+import java.io.IOException;
 
 /**
  * Created by leviathan on 2017.01.03..
@@ -19,6 +23,19 @@ public class ReviewAPIController {
     Returns 5 review URLs with titles
      */
     public String getReviews(Request req, Response res){
-        return apiService.findReviews(req.queryParams(CATEGORY_PARAM_KEY));
+        try {
+            String[][] reviews = apiService.findReviews(req.queryParams(CATEGORY_PARAM_KEY));
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            for (int i=0; i<reviews.length; i++){
+                builder.add(Integer.toString(i), Json.createObjectBuilder()
+                                                        .add("text", reviews[i][0])
+                                                        .add("url", reviews[i][1])
+                                                        .build());
+            }
+            return builder.build().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/main/java/review_finder_service/service/APIService.java
+++ src/main/java/review_finder_service/service/APIService.java
@@ -3,6 +3,13 @@ package review_finder_service.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
  * Created by leviathan on 2017.01.03..
  */
@@ -15,12 +22,31 @@ public class APIService {
 
     public static APIService getInstance() {
         if (INSTANCE == null) {
+            logger.info("Creating new APIService.");
             INSTANCE = new APIService();
         }
+        logger.info("Using existing APIService");
         return INSTANCE;
     }
 
-    public String findReviews(String title){
-        return null;
+    public String[][] findReviews(String title) throws IOException {
+        logger.info("Building URL, trying to connect.");
+        String searchURL = API_URL + "?q=" + title + "+review&num=5";
+        Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+        String[][] reviews = new String[5][2];
+        int idx = 0;
+
+        Elements results = doc.select("h3.r > a");
+        logger.info("Connection successful, parsing URLs");
+        for (Element result : results) {
+            String linkText = result.text();
+            reviews[idx][0] = linkText;
+            String linkHref = result.attr("href");
+            reviews[idx][1] = linkHref.substring(7, linkHref.indexOf("&"));
+            idx++;
+            logger.info(idx + ". URL parsed.");
+            logger.info("Text::" + linkText + ", URL::" + linkHref.substring(7, linkHref.indexOf("&")));
+        }
+        return reviews;
     }
 }
