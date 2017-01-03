523767f550a9a225460f3e017b15d3ba0a2e5dea
annakertesz <kerteszannanak@gmail.com>
2017-01-03 09:59:02 +0100

    skeleton

--- src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
+++ src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
@@ -4,4 +4,19 @@ package delivery_label_generator_service;
  * Created by cave on 2017.01.03..
  */
 public class DeliveryLabelGeneratorController {
+
+    private String qrGenerator(){
+        return "url";
+    }
+
+    private String htmlGenerator(){
+        return "html code";
+    }
+
+    private void pdfCreator(){
+    }
+
+    public String getLabel(){
+        return "response";
+    }
 }
