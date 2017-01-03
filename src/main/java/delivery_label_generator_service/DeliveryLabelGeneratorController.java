1a6473c543b55f055a5647399c81e6ae9f3ee32f
annakertesz <kerteszannanak@gmail.com>
2017-01-03 15:05:39 +0100

    works

--- src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
+++ src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
@@ -1,10 +1,16 @@
 package delivery_label_generator_service;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.codec.Base64;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 /**
@@ -22,7 +28,7 @@ public class DeliveryLabelGeneratorController {
     }
 
     private String htmlGenerator(ArrayList<JSONObject> orderList){
-        String htmlCode = "<html><body>";
+        String htmlCode = "<html style=\"size: 21cm 29.7cm; margin: 30mm 45mm 30mm 45mm;\"><body>";
         for (JSONObject order : orderList) {
             htmlCode += "<div>";
             htmlCode += "<img src=\"" + new QrCodeGenerator(order.getString("id")).getUrlOfQr() + "\" height=\"42\" width=\"42\">";
@@ -33,21 +39,25 @@ public class DeliveryLabelGeneratorController {
         return htmlCode;
     }
 
-//    private void createPDF(){
-//        try {
-//            String k = "<html><body> This is my Project </body></html>";
-//            OutputStream file = new FileOutputStream(new File("C:\\Test.pdf"));
-//            Document document = new Document();
-//            PdfWriter.getInstance(document, file);
-//            document.open();
-//            HTMLWorker htmlWorker = new HTMLWorker(document);
-//            htmlWorker.parse(new StringReader(k));
-//            document.close();
-//            file.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private static String createPDF(){
+        try {
+            String k = "<html><body> This is my project </body></html>";
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Document document = new Document();
+            PdfWriter.getInstance(document, baos);
+            document.open();
+            HTMLWorker htmlWorker = new HTMLWorker(document);
+            htmlWorker.parse(new StringReader(k));
+            document.close();
+            byte[] b = baos.toByteArray();
+            String code = Base64.encodeBytes(b).toString();
+            return "data:application/pdf;base64,"+code;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "fail";
+        }
+    }
 
     public String getLabel(Request request, Response response){
         ArrayList<JSONObject> orders = createListOfJSONObjects(request.queryParams("orders"));
