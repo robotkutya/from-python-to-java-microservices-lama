babaf757eeeb2c48383d67bd50a94c72f2749148
Ádám Horváth <horada1991@gmail.com>
2017-01-03 10:48:48 +0100

    server + createListOfJSONObjects

--- src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
+++ src/main/java/delivery_label_generator_service/DeliveryLabelGeneratorController.java
@@ -1,9 +1,30 @@
 package delivery_label_generator_service;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by cave on 2017.01.03..
  */
 public class DeliveryLabelGeneratorController {
+
+    public String createLabel(Request request, Response response){
+        return "response";
+    }
+
+    private ArrayList<JSONObject> createListOfJSONObjects(String jsonString) {
+        ArrayList<JSONObject> deliveryLabels = new ArrayList<>();
+        JSONArray jsonArray = new JSONArray(jsonString);
+        for (Object object: jsonArray) {
+            deliveryLabels.add(new JSONObject(object.toString()));
+        }
+        return deliveryLabels;
+    }
 
     private String qrGenerator(){
         return "url";
@@ -16,7 +37,7 @@ public class DeliveryLabelGeneratorController {
     private void pdfCreator(){
     }
 
-    public String getLabel(){
-        return "response";
+    public String status(Request request, Response response) {
+        return "OK";
     }
 }
